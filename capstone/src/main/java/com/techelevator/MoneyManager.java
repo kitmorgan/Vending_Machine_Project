fd53399ef7906bb027321dc0d5efd4da3330e3a5
Jeremy Falk <falk.jeremy.r@gmail.com>
2022-06-09 14:05:34 -0400

    Money Manager Change Due with Test

--- capstone/src/main/java/com/techelevator/MoneyManager.java
+++ capstone/src/main/java/com/techelevator/MoneyManager.java
@@ -1,14 +1,14 @@
 package com.techelevator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MoneyManager {
     private BigDecimal currentMoney = new BigDecimal(0);
     private BigDecimal changeDue;
-    public final BigDecimal QUARTER = new BigDecimal(".25");
-    public final BigDecimal DIME = new BigDecimal(".10");
-    public final BigDecimal NICKEL = new BigDecimal(".05");
-    public final BigDecimal PENNY = new BigDecimal(".01");
+
 
     // getters
     public BigDecimal getCurrentMoney() {
@@ -20,11 +20,48 @@ public class MoneyManager {
     }
 
     public void feedMoney(BigDecimal deposit){
-        this.currentMoney.add(deposit);
+        this.currentMoney = this.currentMoney.add(deposit);
     }
 
     public void payFor(BigDecimal price){
         this.currentMoney = this.currentMoney.subtract(price);
+    }
+
+    public Map<String, Integer> coinsDue() {
+        final BigDecimal QUARTER = new BigDecimal(".25");
+        final BigDecimal DIME = new BigDecimal(".10");
+        final BigDecimal NICKEL = new BigDecimal(".05");
+        final BigDecimal PENNY = new BigDecimal(".01");
+        Map<String, Integer> output = new HashMap<>();
+        output.put("QUARTER", 0);
+        output.put("DIME", 0);
+        output.put("NICKEL", 0);
+        output.put("PENNY", 0);
+        if (currentMoney.divide(QUARTER).compareTo(BigDecimal.ONE) >= 0) { // is there at least one quarter owed? .20/.25 >= 1 (false)
+            int quarterDue = getChangeDue().divide(QUARTER, RoundingMode.HALF_DOWN).intValue(); // how many quarters are they owed? 2.00 / .25 = 8
+            output.put("QUARTER", quarterDue); // put it in the map ("QUARTER", 8)
+            BigDecimal removeMe = BigDecimal.valueOf(quarterDue).multiply(QUARTER); // what is the total value of the quarters we are giving them? .25 * 8 = 2.00
+            this.currentMoney = currentMoney.subtract(removeMe); // how much money do we owe after we give them quarters? 2.00 - 2.00 = 0.00
+        }
+        if(currentMoney.divide(DIME).compareTo((BigDecimal.ONE)) >= 0){
+            int dimesDue = getChangeDue().divide(DIME, RoundingMode.HALF_DOWN).intValue();
+            output.put("DIME", dimesDue);
+            BigDecimal removeMe = BigDecimal.valueOf(dimesDue).multiply(DIME);
+            this.currentMoney = currentMoney.subtract(removeMe);
+        }
+        if (currentMoney.divide(NICKEL).compareTo((BigDecimal.ONE)) >= 0) {
+            int nicklesDue = getChangeDue().divide(NICKEL, RoundingMode.HALF_DOWN).intValue();
+            output.put("NICKEL", nicklesDue);
+            BigDecimal removeMe = BigDecimal.valueOf(nicklesDue).multiply(NICKEL);
+            this.currentMoney = currentMoney.subtract(removeMe);
+        }
+        if(currentMoney.compareTo(BigDecimal.ZERO) > 0){
+            int penniesDue = getChangeDue().divide(PENNY,RoundingMode.HALF_DOWN).intValue();
+            output.put("PENNY", penniesDue);
+            BigDecimal removeMe = BigDecimal.valueOf(penniesDue).multiply(PENNY);
+            this.currentMoney = currentMoney.subtract(removeMe);
+        }
+        return output;
     }
 
 
---
+++ capstone/src/test/java/com/techelevator/ChangeTest.java
@@ -0,0 +1,88 @@
+package com.techelevator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public class ChangeTest {
+
+    @Test
+    public void one_returns_four_quarters() {
+        MoneyManager moneyManager = new MoneyManager();
+        moneyManager.feedMoney(BigDecimal.ONE);
+        int expectedQ = 4;
+        int expectedD = 0;
+        int expectedN = 0;
+        int expectedP = 0;
+
+        Map<String, Integer> outputMap = moneyManager.coinsDue();
+        int outcomeQ = outputMap.get("QUARTER");
+        int outcomeD = outputMap.get("DIME");
+        int outcomeN = outputMap.get("NICKEL");
+        int outcomeP = outputMap.get("PENNY");
+        Assert.assertEquals(expectedQ, outcomeQ);
+        Assert.assertEquals(expectedD, outcomeD);
+        Assert.assertEquals(expectedN, outcomeN);
+        Assert.assertEquals(expectedP, outcomeP);
+
+    }
+    @Test
+    public void seventycents_returns_2_quarters_and_2_dimes() {
+        MoneyManager moneyManager = new MoneyManager();
+        moneyManager.feedMoney(new BigDecimal(".70"));
+        int expectedQ = 2;
+        int expectedD = 2;
+        int expectedN = 0;
+        int expectedP = 0;
+
+        Map<String, Integer> outputMap = moneyManager.coinsDue();
+        int outcomeQ = outputMap.get("QUARTER");
+        int outcomeD = outputMap.get("DIME");
+        int outcomeN = outputMap.get("NICKEL");
+        int outcomeP = outputMap.get("PENNY");
+        Assert.assertEquals(expectedQ, outcomeQ);
+        Assert.assertEquals(expectedD, outcomeD);
+        Assert.assertEquals(expectedN, outcomeN);
+        Assert.assertEquals(expectedP, outcomeP);
+    }
+    @Test
+    public void sixtysix_returns_2_quarters_and_1_dimes_1nickel_1penny() {
+        MoneyManager moneyManager = new MoneyManager();
+        moneyManager.feedMoney(new BigDecimal(".66"));
+        int expectedQ = 2;
+        int expectedD = 1;
+        int expectedN = 1;
+        int expectedP = 1;
+
+        Map<String, Integer> outputMap = moneyManager.coinsDue();
+        int outcomeQ = outputMap.get("QUARTER");
+        int outcomeD = outputMap.get("DIME");
+        int outcomeN = outputMap.get("NICKEL");
+        int outcomeP = outputMap.get("PENNY");
+        Assert.assertEquals(expectedQ, outcomeQ);
+        Assert.assertEquals(expectedD, outcomeD);
+        Assert.assertEquals(expectedN, outcomeN);
+        Assert.assertEquals(expectedP, outcomeP);
+    }
+    @Test
+    public void zero_returns_zero_all() {
+        MoneyManager moneyManager = new MoneyManager();
+        moneyManager.feedMoney(new BigDecimal("0"));
+        int expectedQ = 0;
+        int expectedD = 0;
+        int expectedN = 0;
+        int expectedP = 0;
+
+        Map<String, Integer> outputMap = moneyManager.coinsDue();
+        int outcomeQ = outputMap.get("QUARTER");
+        int outcomeD = outputMap.get("DIME");
+        int outcomeN = outputMap.get("NICKEL");
+        int outcomeP = outputMap.get("PENNY");
+        Assert.assertEquals(expectedQ, outcomeQ);
+        Assert.assertEquals(expectedD, outcomeD);
+        Assert.assertEquals(expectedN, outcomeN);
+        Assert.assertEquals(expectedP, outcomeP);
+    }
+}

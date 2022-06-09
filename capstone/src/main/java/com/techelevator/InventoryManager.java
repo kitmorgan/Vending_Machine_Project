9b44ddbd91bc9fefd07476196421389089757c02
Jeremy Falk <falk.jeremy.r@gmail.com>
2022-06-09 15:15:26 -0400

    Inventory Manager Constructor

--- capstone/src/main/java/com/techelevator/InventoryManager.java
+++ capstone/src/main/java/com/techelevator/InventoryManager.java
@@ -1,14 +1,48 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class InventoryManager {
     private Map<String, Item> itemMap = new HashMap<>();//Slot, Slot, Item Name, Price
-    public MoneyManager moneyManager;
+    public MoneyManager moneyManager = new MoneyManager();
+    private File inputFile = new File("logTest.txt");
 
-    public InventoryManager() {
+    public InventoryManager(File inputFile) throws Exception {
+        try (Scanner fileScanner = new Scanner(inputFile);) {
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                String[] lineSplitter = line.split("\\|");
+                String slot = lineSplitter[0];
+                String itemName = lineSplitter[1];
+                String stringPrice = lineSplitter[2];
+                BigDecimal priceBD = new BigDecimal(stringPrice);
+                String itemType = lineSplitter[3];
+                Item item = null;
+                String noise = "";
+
+                if (itemType.equalsIgnoreCase("Chip")) {
+                    item = new Chip(slot, itemName, priceBD);
+                } else if (itemType.equalsIgnoreCase("Candy")) {
+                    item = new Candy(slot, itemName, priceBD);
+                } else if (itemType.equalsIgnoreCase("Drink")) {
+                    item = new Candy(slot, itemName, priceBD);
+                } else if (itemType.equalsIgnoreCase("Gum")) {
+                    item = new Gum(slot, itemName, priceBD);
+                } else {
+                    throw new Exception("could not create new item: " + itemType);
+                }
+                itemMap.put(slot, item);
+
+            }
+        } catch (FileNotFoundException fnf) {
+            System.out.println("file not found");
+        }
 
     }
 
